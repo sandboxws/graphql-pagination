f0889f507a25ee2f8ba60e9cf9695c13da3636aa
Ahmed El.Hussaini <328866+sandboxws@users.noreply.github.com>
2021-11-01 01:29:33 -0400

    Added additional metadata attributes

--- lib/graphql_pagination/collection_metadata_type.rb
+++ lib/graphql_pagination/collection_metadata_type.rb
@@ -1,6 +1,10 @@
 module GraphqlPagination
   class CollectionMetadataType < GraphQL::Schema::Object
     field :current_page, Integer, null: false
+    field :next_page, Integer, null: false
+    field :prev_page, Integer, null: false
+    field :first_page, Integer, null: false, method_name: :first_page?
+    field :last_page, Integer, null: false, method_name: :last_page?
     field :limit_value, Integer, null: false
     field :total_count, Integer, null: false
     field :total_pages, Integer, null: false
