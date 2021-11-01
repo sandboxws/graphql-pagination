9a9d9bdbef3f700a42f50bca7a73ba132f77d7dd
Ahmed El.Hussaini <328866+sandboxws@users.noreply.github.com>
2021-11-01 01:36:28 -0400

    Remove unneeded method_name

--- lib/graphql_pagination/collection_metadata_type.rb
+++ lib/graphql_pagination/collection_metadata_type.rb
@@ -3,8 +3,8 @@ module GraphqlPagination
     field :current_page, Integer, null: false
     field :next_page, Integer, null: false
     field :prev_page, Integer, null: false
-    field :first_page, Integer, null: false, method_name: :first_page?
-    field :last_page, Integer, null: false, method_name: :last_page?
+    field :first_page, Integer, null: false
+    field :last_page, Integer, null: false
     field :limit_value, Integer, null: false
     field :total_count, Integer, null: false
     field :total_pages, Integer, null: false
